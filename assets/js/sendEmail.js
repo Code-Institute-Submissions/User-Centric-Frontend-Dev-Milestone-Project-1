c4c33362e6bd627d1f6126449eca074ccded7f25
MACmedia.DEV <macdev@MACmediaDEVs-MacBook-Pro.local>
2020-01-24 01:03:07 -0500

    contactFormUpdates

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,11 +1,15 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "sendEjs", {
+ 
+
+    emailjs.send("gmail", "sendEjs", {        
         "from_name": contactForm.fullname.value,
         "from_email": contactForm.emailaddress.value,
         "project_summary": contactForm.projectsummary.value
     })
     .then(
         function(response) {
+            
+            
             console.log("SUCCESS", response);
         },
         function(error) {
